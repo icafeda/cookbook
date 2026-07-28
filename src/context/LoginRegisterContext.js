c7cdad43b87eda66c93683099951629d54716845
icafeda <icafeda@outlook.com>
2026-07-28 11:51:01 +0700

    updated LoginContext

--- src/context/LoginRegisterContext.js
+++ src/context/LoginRegisterContext.js
@@ -2,6 +2,7 @@ import { createContext, useContext, useReducer } from "react";
 import { loginRegisterReducer } from "../reducers/loginRegisterReducers";
 import { CartContext } from "./CartContext";
 
+//Lây token và user ra
 const loginRegisterInitiateState = {
   user: JSON.parse(localStorage.getItem("user")) || null,
   token: localStorage.getItem("token") || null,
@@ -18,20 +19,22 @@ export const LoginRegisterProvider = ({ children }) => {
   const { clearCart } = useContext(CartContext);
 
   // -- ACTIONS -- //
-  function login(user, token) {
-    const login = {
-      user,
-      token,
-    };
-    dispatch({
-      type: "LOGIN",
-      payload: {
-        ...login,
-      },
-    });
-  }
+    function login(user, token) {
+      // ⭐ Lưu vào localStorage
+      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem("token", token);
+      // const login = {
+      //   user,
+      //   token,
+      // };
+      dispatch({
+        type: "LOGIN",
+        payload: { user, token },
+      });
+    }
 
-  function setUser(user) {
+    function setUser(user) {
+      localStorage.setItem("user", JSON.stringify(user));
     dispatch({
       type: "SET_USER",
       payload: user,
@@ -40,6 +43,10 @@ export const LoginRegisterProvider = ({ children }) => {
 
   function logout() {
     clearCart();
+    // ⭐ Xóa localStorage
+    localStorage.removeItem("user");
+      localStorage.removeItem("token");
+      
     dispatch({
       type: "LOGOUT",
     });
